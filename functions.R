c063f6246970a6b2207c1c19f2028b7734da5557
Shawn Nason <smnason@yahoo.com>
2020-01-13 10:57:15 -0500

    Changes to increase clarity of variaable names and adding comments

--- functions.R
+++ functions.R
@@ -26,30 +26,62 @@ col_mean <- function (x, removeNA = TRUE) {
   means
 }
 
-
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+#make an object that contains a matrix and can perform operations on that matrix
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+  
+  # This is where we store the cached value once the computation is done
+  cachedMatrix <- NULL
+  
+  # set the value - ie perform the 'set' function
   set <- function(y) {
-    x <<- y
-    m <<- NULL
-    }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
+    inputMatrix <<- y
+    cachedMatrix <<- NULL
+  }
+  
+  # return the original
+  get <- function() inputMatrix
+  
+  # set the inverse matrix
+  setsolve <- function(solve) cachedMatrix <<- solve
+  
+  # get the inverse matrix
+  getsolve <- function() cachedMatrix
+  
+  # create a list of things we can to with the variable
   list(set = set, get = get,
-             setsolve = setsolve,
-getsolve = getsolve)
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-cacheSolve <- function(x, ...) {
-    +x$getsolve()
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-      }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+cacheSolve <- function(inputMatrix, ...) {
+  
+  #set the cached matrix to be the soluion for solving the inverse of the matrix 
+  #   (which may not have happend yet, and so could be NULL)
+  cachedMatrix <- inputMatrix$getsolve()
+  
+  #if the solution is already there, tell the user and return the solution
+  if(!is.null(cachedMatrix)) {
+    
+    message("getting cached data")
+    return(cachedMatrix)
+    
+  }
+  
+  #otherwise, set data to be the input matrix, create the inverse and return it
+  else {
+    #doing both of the next two steps in is not needed but makes it clearer
+    data <- inputMatrix$get()
+    cachedMatrix <- solve(data, ...)
+    
+    #could have been just:
+    #cachedMatrix <- solve(inputMatrix$get(), ...)
+    
+    inputMatrix$setsolve(cachedMatrix)
+    
+    #again, could just be:
+    #inputMatrix$setsolve(solve(inputMatrix$get(), ...))
+    
+    return(cachedMatrix)
+    
+  }
 }
